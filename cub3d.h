017bf7f1e24bdd4a9bc4cb13608a423f81ca5136
Abdelhak Bihe <49686439+MrBihe@users.noreply.github.com>
2023-02-11 19:37:04 +0100

    set struct

--- cub3d.h
+++ cub3d.h
@@ -1,4 +1,40 @@
-struct map {
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abihe <abihe@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/10 18:08:36 by abihe             #+#    #+#             */
+/*   Updated: 2023/02/11 19:30:34 by abihe            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-};
+#ifndef CUB3D_H
+# define CUB3D_H
 
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <string.h>
+# include <fcntl.h>
+
+typedef struct	s_map
+{
+	int		map_fd;
+	char	*north;
+	char	*south;
+	char	*west;
+	char	*east;
+	int		f;
+	int		c;
+	char	*line;
+	int		nb_line;
+	char	**l_map;
+	char	player;
+	int		player_x;
+	int		player_y;
+}	t_map;
+
+
+#endif
